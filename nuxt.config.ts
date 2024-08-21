5d0ba5b0220b4b4dd25d31eda4d2b6256f628c3e
Calvin Simpfendorfer <calvin@nomoss.co>
2024-08-21 17:47:13 +1000

    feat: try a pwa, version 2

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,65 @@
-import { defineNuxtConfig } from "nuxt3";
+// import { defineNuxtConfig } from "nuxt";
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
   components: true,
   css: [`@/assets/css/main.css`],
   app: {
-    header: {
-      link: [
-        { rel: "icon", type: "image/x-icon", href: "icons/favicon.ico" },
-        { rel: "manifest", href: "/manifest.json" },
+    // head: {
+    //   link: [
+    //     //{ rel: "icon", type: "image/x-icon", href: "icons/favicon.ico" },
+    //     //{ rel: "manifest", href: "/manifest.json" },
+    //   ],
+    // },
+  },
+
+  modules: ["@vite-pwa/nuxt"],
+  pwa: {
+    manifest: {
+      name: "Super Tennis Availability",
+      short_name: "Tennis",
+      // start_url: "https://super-tennis-availability.vercel.app/",
+      description: "A Tennis Court Availability Frontend",
+      screenshots: [
+        {
+          src: "screenshot-1280x720.png",
+          type: "image/png",
+          sizes: "1280x720",
+          form_factor: "wide",
+        },
+        {
+          src: "screenshot-375x667.png",
+          type: "image/png",
+          sizes: "375x667",
+          form_factor: "narrow",
+        },
       ],
+      icons: [
+        {
+          src: "icons/android-chrome-512x512.png",
+          sizes: "512x512",
+          type: "image/png",
+        },
+        {
+          src: "icons/android-chrome-192x192.png",
+          sizes: "192x192",
+          type: "image/png",
+        },
+        {
+          src: "icons/apple-touch-icon.png",
+          sizes: "180x180",
+          type: "image/png",
+        },
+      ],
+      theme_color: "white",
+      background_color: "#5d9caa",
     },
+
+    workbox: {
+      navigateFallback: "/",
+    },
+
+    // For DevTools debugging... I think?
+    devOptions: { enabled: true, type: "module" },
   },
 });
